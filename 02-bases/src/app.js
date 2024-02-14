6099101c6bd84a39f84973741603d7ec08991f0c
Gabriela Flores <gabriela.floze@gmail.com>
2024-02-14 13:11:03 -0600

    add a javascript review - async/await

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -3,7 +3,7 @@ const getPokemonById = require('./js-foundation/06-promises');
 
 getPokemonById(23)
     .then((pokemon) => console.log({ pokemon }))
-    .catch((err) => console.log({ err }))
+    //.catch((err) => console.log({ err }))
     .finally(() => console.log('Finalmente'));
 
 
--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,11 +1,17 @@
 
-const getPokemonById = (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+const getPokemonById = async (id) => {
+     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    return fetch(url)
-        .then((response) => response.json())
-        //.then(() => { throw new Error('Pokemon no existe') })
-        .then((pokemon) => pokemon.name)
+    const resp = await fetch(url);
+    const pokemon = await resp.json();
+
+    //throw new Error('Pokemon no existe');
+    return pokemon.name;
+
+    // return fetch(url)
+    //     .then((response) => response.json())
+    //     //.then(() => { throw new Error('Pokemon no existe') })
+    //     .then((pokemon) => pokemon.name)
 }
 
 module.exports = getPokemonById;
