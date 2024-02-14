9ed901c1650da5b6415df99af53195de58a235c6
Gabriela Flores <gabriela.floze@gmail.com>
2024-02-14 15:34:36 -0600

    axios - http client

--- 02-bases/src/plugins/http-client-plugin.js
+++ 02-bases/src/plugins/http-client-plugin.js
@@ -1,15 +1,17 @@
 
+const axios = require('axios');
+
 const httpClientPlugin = {
-    get: async(url)=> {
-        const resp = await fetch(url);
-        return await resp.json();
+    get: async (url) => {
+        const { data } = await axios.get(url);
+        return data;
     },
 
-    post: async(url,body)=> {},
-    put: async(url,body)=> {},
-    delete: async(url)=> {},
+    post: async (url, body) => { },
+    put: async (url, body) => { },
+    delete: async (url) => { },
 };
 
-module.exports ={
+module.exports = {
     http: httpClientPlugin
 };
