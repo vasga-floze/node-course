7c504fa8e2e8d65fc0c10d49b69bf6109290132a
Gabriela Flores <gabriela.floze@gmail.com>
2024-02-16 10:28:02 -0600

    using TypeScript in Node

---
+++ 03-typescript/dist/services/hero.service.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findHeroById = void 0;
+const heroes_1 = require("../data/heroes");
+const findHeroById = (id) => {
+    return heroes_1.heroes.find((hero) => hero.id == id);
+};
+exports.findHeroById = findHeroById;
