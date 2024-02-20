c70e1711740e4a32d0584d6a2b1585be457bb397
Gabriela Flores <gabriela.floze@gmail.com>
2024-02-20 13:05:00 -0600

    test 03 (callback blocking)

--- 02-bases/src/js-foundation/03-callbacks.ts
+++ 02-bases/src/js-foundation/03-callbacks.ts
@@ -19,9 +19,13 @@ export const getUserById = (id: number, callback: (err?: string, user?: User) =>
 
     const user = users.find((user) => user.id === id);
 
-    (user)
-        ? callback(undefined, user)
-        : callback(`User not found with id ${id}`);
+    if (!user) {
+        setTimeout(() => {
+            callback(`User not found with id ${id}`);
+        }, 2500);
+
+        return;
+    }
 }
 
 //getUserById(1);
--- 02-bases/tests/js-foundation/03-callbacks.test.ts
+++ 02-bases/tests/js-foundation/03-callbacks.test.ts
@@ -3,13 +3,15 @@ import { getUserById } from '../../src/js-foundation/03-callbacks';
 
 describe('js-foundation/03-callbacks', () => {
     
-    test('Return an error if user id does not exist', () => {
+    test('Return an error if user id does not exist', (done) => { //using done to track if callbacks is completed
 
         const id = 10;
 
         getUserById(id, (err, user) => { //this callback is non blocking
             expect(err).toBe(`User not found with id ${id}`);
             expect(user).toBeUndefined();
+
+            done(); //let us know the response its done
         });
     });
 });
