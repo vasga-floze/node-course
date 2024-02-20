9bc275753e546dd83dc11d285439668877660434
Gabriela Flores <gabriela.floze@gmail.com>
2024-02-20 09:46:14 -0600

    configuring test environment

---
+++ 02-bases/tests/app.test.ts
@@ -0,0 +1,15 @@
+// A  A  A
+
+describe('Test in the App file', () => {
+    test('Should be 30', () => { 
+        //1. Arrange
+        const num1 = 10;
+        const num2 = 20;
+
+        //2. Act
+        const result = num1 + num2;
+
+        //3. Assert
+        expect(result).toBe(30);
+    });
+});
