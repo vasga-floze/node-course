233b0b150b0234aaa30c5507683486ca9bfe1d25
Gabriela Flores <gabriela.floze@gmail.com>
2024-02-23 12:01:28 -0600

    test 01 plugin (spy and mock)

--- 02-bases/src/plugins/get-age.plugin.ts
+++ 02-bases/src/plugins/get-age.plugin.ts
@@ -2,6 +2,7 @@
 
 export const getAge = (birthdate: string) => {
 
+    console.log({ currentYear: new Date().getFullYear() })
     // return getAgePlugin(birthdate);
     return new Date().getFullYear() - new Date(birthdate).getFullYear();
 }
--- 02-bases/tests/plugins/get-age.plugins.test.ts
+++ 02-bases/tests/plugins/get-age.plugins.test.ts
@@ -1,14 +1,14 @@
 import { getAge } from '../../src/plugins/get-age.plugin';
 
-describe('plugins/get-age.plugin', ()=>{
-    test('getAge() should return the age of a person (a number)', ()=>{
+describe('plugins/get-age.plugin', () => {
+    test('getAge() should return the age of a person (a number)', () => {
         const birthdate = '1985-10-21';
         const age = getAge(birthdate);
 
         expect(typeof age).toBe('number');
     });
 
-    test('getAge() should return current age', () => {   
+    test('getAge() should return current age', () => {
 
         const birthdate = '1985-10-21';
         const age = getAge(birthdate);
@@ -17,4 +17,17 @@ describe('plugins/get-age.plugin', ()=>{
 
         expect(age).toBe(calculatedAge);
     });
+
+    //espia sobre un metodo
+    //spyOn() should be able to mock methods
+    test('getAge should return 0 years old', () => {
+        const spy = jest.spyOn(Date.prototype, 'getFullYear').mockReturnValue(1995);
+
+        const birthdate = '1995-10-21';
+        const age = getAge(birthdate);
+
+        expect(age).toBe(0);
+        expect(spy).toHaveBeenCalledWith();
+
+    })
 });
