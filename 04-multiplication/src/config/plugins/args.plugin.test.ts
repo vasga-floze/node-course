e5331bbf3ede1023911f07c1daa6f494102819a6
Gabriela Flores <gabriela.floze@gmail.com>
2024-03-06 15:49:32 -0600

    section 7 - test Argv custom values

--- 04-multiplication/src/config/plugins/args.plugin.test.ts
+++ 04-multiplication/src/config/plugins/args.plugin.test.ts
@@ -8,7 +8,14 @@ const runCommand = async (args: string[]) => {
 
 describe('Test ars.plugin.ts', () => {
 
-    test('Should return default values', async()=> {
+    const originalArgv = process.argv;
+
+    beforeEach(()=>{
+        process.argv = originalArgv;
+        jest.resetModules();
+    });
+
+    test('Should return default values', async () => {
 
         const argv = await runCommand(['-b', '5']);
         //console.log(argv);
@@ -22,4 +29,19 @@ describe('Test ars.plugin.ts', () => {
         }));
 
     });
+
+    test('Should return configuration with custom values', async () => {
+
+        const argv = await runCommand(['-b', '8', '-l', '20', '-s', '-n', 'custom-name', '-d', 'custom-dir']);
+
+        expect(argv).toEqual(expect.objectContaining({
+            b: 8,
+            l: 20,
+            s: true,
+            n: 'custom-name',
+            d: 'custom-dir',
+        }));
+
+    });
+
 });
