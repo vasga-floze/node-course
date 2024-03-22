521a3b433c8fa52d8bfbcb1ff046913a84ccda1e
Gabriela Flores <gabriela.floze@gmail.com>
2024-03-22 11:30:27 -0600

    section 11 - node + mongo - mongoose

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,11 +1,19 @@
 import { envs } from "./config/plugins/env.plugings";
+import { MongoDatabase } from "./data/mongo/init";
 import { Server } from "./presentation/server";
 
 (async () => {
     main();
 })();
 
-function main() {
-    Server.start();
+async function main() {
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName:envs.MONGO_DB_NAME
+    })
+
+
+    //Server.start();
     //console.log(envs);
 }
