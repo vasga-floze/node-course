396ec872679296f6dee170dff313a6b3a4e034ed
Gabriela Flores <gabriela.floze@gmail.com>
2024-03-25 12:03:11 -0600

    section 11 - schema and models - create and read mongo

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,5 +1,6 @@
 import { envs } from "./config/plugins/env.plugings";
 import { MongoDatabase } from "./data/mongo/init";
+import { LogModel } from "./data/mongo/models/log.model";
 import { Server } from "./presentation/server";
 
 (async () => {
@@ -10,9 +11,20 @@ async function main() {
 
     await MongoDatabase.connect({
         mongoUrl: envs.MONGO_URL,
-        dbName:envs.MONGO_DB_NAME
+        dbName: envs.MONGO_DB_NAME
     })
 
+
+    // // Crear una coleccion = tables y documento = registro
+    // const newLog = await LogModel.create({
+    //     message: 'Test message',
+    //     origin: 'App.ts',
+    //     level: 'low',
+
+    // });
+
+    const logs = await LogModel.find();
+    console.log(logs[0].message);
 
     //Server.start();
     //console.log(envs);
